a730ee3d53c2343e1db6ab3b53ae757776e4aa84
dirkarnez <smalldirkalex@gmail.com>
2023-09-07 18:51:37 +0800

    - init

---
+++ README.md
@@ -0,0 +1,9 @@
+
+### Notes
+- std::locale lookes for installed locales
+- use gettext or icu for generate exotic locale to binary files (resource bundles)
+- Console API (e.g `CONSOLE_FONT_INFOEX`) on Windows is deprecated. 
+
+### TODOS
+- [ ] https://github.com/unicode-org/icu/blob/main/icu4c/source/data/locales/my.txt
+- [ ] gettext tools
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
+#include <windows.h>
 #include <locale>
 
-int main()
-{
-    // Set the console output encoding to UTF-8
-    std::locale::global(std::locale("en_US.UTF-8"));
 
-    // Print Burmese text to the console
-    std::wcout << L"မင်္ဂလာပါ" << std::endl;
+int main() {
+    {
+        // Get the handle to the console window
+        HWND hwnd = GetConsoleWindow();
+
+        // Get the handle to the console's output buffer
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+        // Set the font of the console window to "Consolas"
+        CONSOLE_FONT_INFOEX fontInfo = {0};
+        fontInfo.cbSize = sizeof(fontInfo);
+        fontInfo.dwFontSize.Y = 16;
+        wcscpy_s(fontInfo.FaceName, L"Consolas");
+        SetCurrentConsoleFontEx(hConsole, FALSE, &fontInfo);
+    }
+
+    {
+        // don't know what is it
+        // const std::ctype<wchar_t>& my_MM_facet = std::use_facet<std::ctype<wchar_t> >(std::locale(std::locale(), new my_MM_UTF_8));
+
+        // Set the console output encoding to UTF-8
+        std::locale::global(std::locale("my_MM.UTF-8"));
+
+        // Print Burmese text to the console
+        // should fail, not every system has this locale
+        std::wcout << L"မင်္ဂလာပါ" << std::endl; 
+    }
+
+    // Output a message in the console window
+    std::cout << "Hello, world!" << std::endl;
 
     return 0;
 }
